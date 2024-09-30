2ba3a3fd6e989bcd7ebbb30845c248dc5fd4831a
SayanMazumdar <sayanmazumdar355@outlook.com>
2024-10-01 00:06:23 +0530

    Developed the UI of the CityDetails component

--- src/components/CityDetails.jsx
+++ src/components/CityDetails.jsx
@@ -11,27 +11,26 @@ const tempData = {
 export default function CityDetails() {
   return (
     <div className={styles.layout}>
-      <div className={styles.Row}>
-        <p>CITY NAME</p>
-        <div className={styles.firstCol}>
+      <div className={styles.row}>
+        <p className={styles.label}>CITY NAME</p>
+        <div className={styles.city}>
           {EmojiToImg(tempData.emoji)}
           <p>{tempData.city}</p>
         </div>
       </div>
-      <div className={styles.Row}>
-        <p>YOU WENT TO BERLIN ON</p>
-        <p>📅 {new Date(tempData.date).toLocaleDateString('en-US',{ weekday: "short", day: "2-digit", month: "long", year: "numeric"})}</p>
+      <div className={styles.row}>
+        <p className={styles.label}>YOU WENT TO BERLIN ON</p>
+        <p className={styles.field}>📅 {new Date(tempData.date).toLocaleDateString('en-US', { weekday: "long", day: "2-digit", month: "long", year: "numeric" })}</p>
       </div>
-      <div className={styles.Row}>
-        <p>YOUR NOTES</p>
-        <p>{tempData.notes}</p>
+      <div className={styles.row}>
+        <p className={styles.label}>YOUR NOTES</p>
+        <p className={styles.field}>{tempData.notes}</p>
       </div>
-      <div className={styles.Row}>
-        <p>LEARN MORE</p>
-        <a href={`https://en.wikipedia.org/wiki/${tempData.city.toLowerCase()}`} target='blank'>Check out Berlin on Wikipedia &rarr;</a>
+      <div className={styles.row}>
+        <p className={styles.label}>LEARN MORE</p>
+        <a href={`https://en.wikipedia.org/wiki/${tempData.city.toLowerCase()}`} target='blank' className={`${styles.field} ${styles.link}`}>Check out Berlin on Wikipedia &rarr;</a>
       </div>
-      <button type="submit">&larr; BACK</button>
-
+      <button className='btnTransparent'>&larr; BACK</button>
     </div>
   )
 }
